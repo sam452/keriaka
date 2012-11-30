76c58e4e5602611b589e42b7a3f344af1c510b88
sam walton <sam@foofrancine.com>
2012-11-29 21:37:49 -0600

    test precompile

--- config/application.rb
+++ config/application.rb
@@ -52,9 +52,11 @@ module Keriaka
     # in your app. As such, your models will need to explicitly whitelist or blacklist accessible
     # parameters by using an attr_accessible or attr_protected declaration.
     config.active_record.whitelist_attributes = true
+    
 
     # Enable the asset pipeline
-    config.assets.enabled = true
+     config.assets.enabled = true
+    # config.assets.initialize_on_precompile = false
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
